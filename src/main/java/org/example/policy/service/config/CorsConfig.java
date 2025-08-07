6bdf56fee12f122cfe7e3c8290fce593109dfc4c
Sahney <Dilpreet.Sahney@wolterskluwer.com>
2025-08-07 22:07:11 +0530

    chore: add cors configuration

---
+++ src/main/java/org/example/policy/service/config/CorsConfig.java
@@ -0,0 +1,27 @@
+package org.example.policy.service.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.List;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.setAllowedOriginPatterns(List.of("*")); // or specific origins
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config); // Apply to all routes
+
+        return new CorsFilter(source);
+    }
+}
